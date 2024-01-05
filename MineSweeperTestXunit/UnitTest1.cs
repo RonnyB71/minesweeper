7d888f67d6a56c9b93cb282ef923affa1d463b3e
espenhalsen <espylv@gmail.com>
2024-01-05 11:01:31 +0100

    first commit, 1 error passed

---
+++ MineSweeperTestXunit/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace MineSweeperTestXunit
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
--- Minesweeper/Implementation/StaticBoardGenerator.cs
+++ Minesweeper/Implementation/StaticBoardGenerator.cs
@@ -1,26 +1,30 @@
 ﻿using Minesweeper.Models;
+using System;
+using System.Linq;
 
-namespace Minesweeper.Implementation;
-
-public class StaticBoardGenerator : IBoardGenerator
+namespace Minesweeper.Implementation
 {
-    /// <summary>
-    /// Returns a board based on a board definition string.
-    /// The string defines the board as a grid of characters
-    /// where 'm' represents a mine, '.' represents a blank tile.
-    /// Each row is separated by a comma. Each row should have 
-    /// equal amount of columns.
-    /// </summary>
-    /// <param name="boardDefinition">A string that defines the board.</param>
-    /// <example>.......,..mm...,......m,m...m..,..m.m..,.mmm...,.......</example>
-    public StaticBoardGenerator(string boardDefinition) 
+    public class StaticBoardGenerator : IBoardGenerator
     {
-        // TODO: Implement this constructor
-    }
-    public Board GenerateBoard()
-    {
-        //TODO: Implement this method
+        private readonly string _boardDefinition;
 
-        return new Board(0, 0, 0);
+        public StaticBoardGenerator(string boardDefinition) 
+        {
+            if (string.IsNullOrWhiteSpace(boardDefinition))
+                throw new ArgumentException("Board definition cannot be null or empty.");
+
+            var rows = boardDefinition.Split(',');
+            if (rows.Length == 0) 
+                throw new ArgumentException("Board must have at least one row.");
+
+
+            _boardDefinition = boardDefinition;
+        }
+
+        public Board GenerateBoard()
+        {
+          
+            return new Board(0, 0, 0); 
+        }
     }
 }
